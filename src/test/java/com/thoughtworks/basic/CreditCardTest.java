8fff98a736dc768d0fd8d004c775248c20835d1e
pengyu <604131805@.com>
2020-07-25 10:01:24 +0800

    补充需求1

--- src/test/java/com/thoughtworks/basic/CreditCardTest.java
+++ src/test/java/com/thoughtworks/basic/CreditCardTest.java
@@ -15,11 +15,33 @@ public class CreditCardTest {
     }
 
     @Test
-    public void should_return_0_points_when_cost_9_by_pos(){
+    public void should_return_1_points_when_cost_108_by_pos(){
         Pay posPay = new PosPay();
 
-        int points = posPay.cost(9);
+        int points = posPay.cost(108);
+
+        Assert.assertEquals(10 ,points);
+    }
+
+    @Test
+    public void should_return_1_points_when_cost_208_by_pos(){
+        Pay posPay = new PosPay();
+
+        int points = posPay.cost(208);
+
+        Assert.assertEquals(20 ,points);
+    }
+
+    @Test
+    public void should_return_0_points_when_cost_8_by_pos(){
+        Pay posPay = new PosPay();
+
+        int points = posPay.cost(8);
 
         Assert.assertEquals(0 ,points);
     }
+
+
+
+
 }
