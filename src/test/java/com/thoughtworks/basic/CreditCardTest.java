79e09b4d9619c0274f6315e9544011bff2cf3864
pengyu <604131805@.com>
2020-07-25 09:58:12 +0800

    Pos消费不足1积分得0积分

--- src/test/java/com/thoughtworks/basic/CreditCardTest.java
+++ src/test/java/com/thoughtworks/basic/CreditCardTest.java
@@ -14,5 +14,12 @@ public class CreditCardTest {
         Assert.assertEquals(1 ,points);
     }
 
+    @Test
+    public void should_return_0_points_when_cost_9_by_pos(){
+        Pay posPay = new PosPay();
 
+        int points = posPay.cost(9);
+
+        Assert.assertEquals(0 ,points);
+    }
 }
